02cba6e76e8e8816b3d8cfc50536aaa96bf03f97
Raniere <i4376@rn.senac.br>
2026-07-28 14:43:34 -0300

    Feature: Header - Adicionado o código base do Header contendo o menu de navegação e o botão que levará à rota de Doações.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Link from "next/link";
 
 export default function Header() {
@@ -5,17 +7,59 @@ export default function Header() {
     return (
 
         <header className="header">
+
+
             <div className="header-container">
+
 
                 <div className="logo">
                     <Link href="/"> VivaPet </Link>
                 </div>
 
+
                 <nav className="menu">
+
+                    <div className="menu-item">
+                        <span>Animais</span>
+
+                        <div className="submenu">
+                            <Link href="/animais/cadastrar"> Cadastrar novo animal </Link>
+                            <Link href="/animais/necessidades"> Cadastrar necessidades do animal </Link>
+                        </div>
+
+                    </div>
+
+
+                    <div className="menu-item">
+                        <span>Cuidadores</span>
+
+                        <div className="submenu">
+                            <Link href="/cuidadores/cadastrar"> Cadastrar novo cuidador </Link>
+                            <Link href="/cuidadores/vincular"> Vincular cuidador </Link>
+                        </div>
+                        
+                    </div>
+
+
+                    <div className="menu-item">
+                        <span>Suprimentos</span>
+
+                        <div className="submenu">
+                            <Link href="/suprimentos/cadastrar"> Cadastrar novo suprimento </Link>
+                            <Link href="/suprimentos/checar"> Checar suprimento </Link>
+                        </div>
+                        
+                    </div>
 
                 </nav>
 
+
+                <div className="donations">
+                    <Link href="/doacoes"> Doações </Link>
+                </div>
+
             </div>
+
         </header>
     );
 }
